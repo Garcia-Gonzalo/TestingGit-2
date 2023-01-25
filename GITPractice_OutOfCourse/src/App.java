5ff848783f0746d2fc8dc24f601d56e2a4b27dd8
GonzaloGarcia <gonzalopgarcia@gmail.com>
2023-01-25 20:06:24 -0300

    Se realizo el 1er commit de la rama developer
    Clase Persona

--- GITPractice_OutOfCourse/src/App.java
+++ GITPractice_OutOfCourse/src/App.java
@@ -1,7 +1,11 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Esta es la rama principal");
 
         System.out.println("El primer cambio en la rama principal");
+
+        Persona p1 = new Persona("Gonzalo", 47);
+        System.out.println(p1);
     }
 }
