e5a88dd1fd032a26813152e250e7f2936fcd2b2b
GonzaloGarcia <gonzalopgarcia@gmail.com>
2023-01-25 19:41:48 -0300

    Es el segundo commit
    Se realizó agregado de una nueva linea

--- GITPractice_OutOfCourse/src/App.java
+++ GITPractice_OutOfCourse/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Esta es la rama principal");
+
+        System.out.println("El primer cambio en la rama principal");
     }
 }
